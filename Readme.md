0318d2f943ab4eee5b73cf3eaae536bf8c99e19e
MIRABEAU\mquekel <mquekel@mirabeau.nl>
2018-07-10 18:34:08 +0200

    Add some readme.

---
+++ Readme.md
@@ -0,0 +1,18 @@
+# Rockstars developers exercise
+
+This is the implementation of the Rockstars developers exercise.
+
+## Usage
+This API can be used for importing songs and artists into the database.
+Artists can only be imported after importing songs for the artist.
+
+## Install
+To install the solution, follow these steps:
+
+* Pull the sources from Github
+* Create a new SQL Database
+* Import the SQL Tables by executing the queries found in SQL/Create-Tables.sql
+* Set the connectionstring in Rockstars.WebAPI/appsettings.json
+
+## Documentation
+The API is documented by Swagger.
---
+++ Rockstars.DataAccess.Tests/Services/Import/ArtistAlreadyExistsRuleTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using FakeItEasy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rockstars.DataAccess.Models;
+using Rockstars.DataAccess.Repositories;
+using Rockstars.DataAccess.Services.Import;
+using Rockstars.Domain.Entities;
+using Should;
+
+namespace Rockstars.DataAccess.Tests.Services.Import
+{
+    [TestClass]
+    public class ArtistAlreadyExistsRuleTests
+    {
+        private readonly IRepository<Artist> _artistRepository;
+
+        private readonly ArtistAlreadyExistRule _Rule;
+
+        public ArtistAlreadyExistsRuleTests()
+        {
+            _artistRepository = A.Fake<IRepository<Artist>>();
+            _Rule = new ArtistAlreadyExistRule(_artistRepository);
+        }
+
+        [TestMethod]
+        public void ArtistAlreadyExits()
+        {
+            // assign
+            var artistName = "Rolling Stones";
+            var artist = new Artist {Id = 1234, Name = artistName};
+
+            A.CallTo(() => _artistRepository.Search(A<Func<Artist, bool>>._))
+                .Returns(new[] {new Artist {Name = artistName}});
+
+            // act
+            var validationResult = this._Rule.IsValid(artist);
+
+            // assert
+            validationResult.Status.ShouldEqual(ValidationStatus.Failed);
+        }
+
+        [TestMethod]
+        public void ArtistDoesNotExist()
+        {
+            // assign
+            var artistName = "Rolling Stones";
+            var artist = new Artist {Id = 1234, Name = artistName};
+
+            var noResults = Enumerable.Empty<Artist>();
+            A.CallTo(() => _artistRepository.Search(A<Func<Artist, bool>>._))
+                .Returns(noResults);
+
+            // act
+            var validationResult = this._Rule.IsValid(artist);
+
+            // assert
+            validationResult.Status.ShouldEqual(ValidationStatus.Succeeded);
+        }
+    }
+}
